e365218455b25b4761b76ff031371764142df07c
Roman Prybluda <108694753+RomanPrybluda@users.noreply.github.com>
2025-10-09 11:20:58 +0300

    add settings

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,8 @@
-// env.d.ts (в корне проекта или в src/)
+
 declare namespace NodeJS {
   interface ProcessEnv {
     GOOGLE_CLIENT_EMAIL: string;
     GOOGLE_PRIVATE_KEY: string;
-    GOOGLE_IMPERSONATE_SUBJECT?: string;
-    GOOGLE_ROOT_FOLDER_ID?: string;
+    GOOGLE_IMPERSONATE_SUBJECT?: string;    
   }
 }
